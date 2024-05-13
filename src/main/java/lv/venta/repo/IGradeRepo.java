3e204242c895db39945584c76fc63b4bcf1983aa
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-13 12:09:40 +0300

    Add: Grade filter service - class

--- src/main/java/lv/venta/repo/IGradeRepo.java
+++ src/main/java/lv/venta/repo/IGradeRepo.java
@@ -8,6 +8,8 @@ import lv.venta.model.Grade;
 
 public interface IGradeRepo extends CrudRepository<Grade, Integer>{
 
+	//pēc noklusejuma public abstract
+	//izpildīs so vaicājumu: select * from grade_table where grvalue< <argumentā padoto>;
 	ArrayList<Grade> findByGrvalueLessThan(int i);
 
 }
