91b992ea2d0cfccdb4686c18343080c7575d0d9b
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-13 09:48:51 +0300

    Update: added Grade class with  OneToMany linkage between Grade and Course

--- src/main/java/lv/venta/model/Course.java
+++ src/main/java/lv/venta/model/Course.java
@@ -1,11 +1,14 @@
 package lv.venta.model;
 
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
@@ -48,6 +51,11 @@ public class Course {
 	@OneToOne
 	@JoinColumn(name = "Idp")//Professor klases kolonnas nosaukums
 	private Professor professor;
+	
+	@OneToMany(mappedBy = "course")
+	@ToString.Exclude
+	private Collection<Grade> grades;
+	
 	
 	public Course(String title, int creditpoints, Professor professor) {
 		setTitle(title);
