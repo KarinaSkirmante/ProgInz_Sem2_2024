dee2ce7445e9a1ccbde71bbf5b462a84d2c85d36
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-17 10:34:43 +0300

    Add: update course filter controller and new html page

--- src/main/java/lv/venta/controller/CourseFilterController.java
+++ src/main/java/lv/venta/controller/CourseFilterController.java
@@ -37,5 +37,42 @@ public class CourseFilterController {
 		
 	}
 	
+	@GetMapping("/filter/professor/{id}")//localhost:8080/course/filter/professor/2
+	public String getCourseFilterByProfessorId(@PathVariable("id") int id, Model model) {
+		
+		try
+		{
+			Course selectedCourse = courseFilterService.selectCourseByProfessorId(id);
+			model.addAttribute("mydata", selectedCourse);//padodam izfiltrētos kursus uz course-show-one-page.html lapu
+			model.addAttribute("msg", "Course filtered by professor");
+			return "course-show-one-page";//parādām pašu course-show-one-page.html lapu interneta pārlūkā
+		}catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());//padod kļudas ziņu uz error-page.html lapu
+			return "error-page";//parādām pašu error-page.html lapu interneta pārlūkā
+		}
+		
+	}
+	
+	
+	
+	
 
+	@GetMapping("/filter/student/{id}")//localhost:8080/course/filter/student/2
+	public String getCourseFilterByStudentId(@PathVariable("id") int id, Model model) {
+		
+		try
+		{
+			ArrayList<Course> selectedCourses = courseFilterService.selectCoursesByStudentId(id);
+			model.addAttribute("mydata", selectedCourses);//padodam izfiltrētos kursus uz course-show-all-page.html lapu
+			model.addAttribute("msg", "Courses filtered by student id");
+			return "course-show-all-page";//parādām pašu course-show-all-page.html lapu interneta pārlūkā
+		}catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());//padod kļudas ziņu uz error-page.html lapu
+			return "error-page";//parādām pašu error-page.html lapu interneta pārlūkā
+		}
+		
+	}
+	
+	
+	
 }
