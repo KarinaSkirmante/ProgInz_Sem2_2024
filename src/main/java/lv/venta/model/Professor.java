a56c877825e65dc0a764368238e8e022b7393bf3
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-17 11:44:53 +0300

    Update: created manytomany linkage between rpofessor and Course

--- src/main/java/lv/venta/model/Professor.java
+++ src/main/java/lv/venta/model/Professor.java
@@ -1,10 +1,16 @@
 package lv.venta.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -46,14 +52,26 @@ public class Professor {
 	@Column(name="Degree")
 	private Degree degree;
 	
-	@OneToOne(mappedBy = "professor")//Course klases mainīgā nosaukums
+	@ManyToMany
+	@JoinTable(name= "ProfessorCourseTable",
+	joinColumns = @JoinColumn(name = "Idp"),
+	inverseJoinColumns = @JoinColumn(name = "Idc"))
 	@ToString.Exclude
-	private Course course;
-	
+	private Collection<Course> courses = new ArrayList<>();
 
 	public Professor(String name, String surname, Degree degree) {
 		setName(name);
 		setSurname(surname);
 		setDegree(degree);
 	}
+	
+	public void addCourse(Course course) {
+		if(!courses.contains(course))
+			courses.add(course);
+	}
+	
+	//TODO uztaisīt arī izņemšanas fukciju
+	
+	
+	
 }
