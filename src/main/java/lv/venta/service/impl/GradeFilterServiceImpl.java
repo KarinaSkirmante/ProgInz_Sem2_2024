411dbd6a429ebf52060de70e7f61c5bd739ebff4
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-17 09:46:03 +0300

    Update: completed GradeFilterServiceImpl with data jpa functions in IGradeRepo

--- src/main/java/lv/venta/service/impl/GradeFilterServiceImpl.java
+++ src/main/java/lv/venta/service/impl/GradeFilterServiceImpl.java
@@ -3,10 +3,13 @@ package lv.venta.service.impl;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import lv.venta.model.Grade;
+import lv.venta.repo.ICourseRepo;
 import lv.venta.repo.IGradeRepo;
+import lv.venta.repo.IStudentRepo;
 import lv.venta.service.IGradeFilterService;
 
 @Service
@@ -14,6 +17,12 @@ public class GradeFilterServiceImpl implements IGradeFilterService{
 
 	@Autowired
 	private IGradeRepo gradeRepo;
+	
+	@Autowired
+	private IStudentRepo studRepo;
+	
+	@Autowired
+	private ICourseRepo courseRepo;
 	
 	@Override
 	public ArrayList<Grade> selectFailedGrades() throws Exception {
@@ -26,14 +35,28 @@ public class GradeFilterServiceImpl implements IGradeFilterService{
 
 	@Override
 	public ArrayList<Grade> selectGradesByStudentId(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if(id < 1) throw new Exception("Id should be positive");
+		
+		if(!studRepo.existsById(id)) throw new Exception("Student with " + id + "doesn't exists");
+		
+		
+		ArrayList<Grade> result = gradeRepo.findByStudentIds(id);
+		if(result.isEmpty()) throw new Exception("There is no grade for this student");
+		
+		return result;
 	}
 
 	@Override
 	public float calculateAVGGradeInCourseId(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		// TODO
+		if(id < 1) throw new Exception("Id should be positive");
+		if(!courseRepo.existsById(id)) throw new Exception("Course with " + id + "doesn't exists");
+		
+		float result = gradeRepo.calculateAVGGradeInCourseById(id);
+		
+		if(result == 0) throw new Exception("There is no grade for this course");
+
+		return result;
 	}
 
 }
