729051d11ee097201f17f9eea9adf93f06c3b49d
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-17 11:18:18 +0300

    Update: completed grade filter controller

--- src/main/java/lv/venta/controller/GradeFilterController.java
+++ src/main/java/lv/venta/controller/GradeFilterController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lv.venta.model.Grade;
@@ -35,6 +36,38 @@ public class GradeFilterController {
 		}
 	}
 	
+	
+	@GetMapping("/student/{id}")//localhost:8080/grade/filter/student/1
+	public String getGradeFilterByStudentId(@PathVariable("id") int id, Model model) {
+		try
+		{
+			ArrayList<Grade> selectGrades = gradeFilterService.selectGradesByStudentId(id);
+			model.addAttribute("mydata", selectGrades);//padodam datus uz grade-show-all-page.html lapu
+			model.addAttribute("msg", "Filtered grades by student");
+			return "grade-show-all-page";//tiks parādīt grade-show-all-page.html lapa interneta pārlūka
+		}
+		catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());//padod kļudas ziņu uz error-page.html lapu
+			return "error-page";//parādām pašu error-page.html lapu interneta pārlūkā
+
+		}
+	}
+	
+	
+	@GetMapping("/avg/course/{id}")//localhost:8080/grade/filter/avg/course/1
+	public String getGradeFilterAVGByCourseId(@PathVariable("id") int id, Model model) {
+		try
+		{
+			float avgGrades = gradeFilterService.calculateAVGGradeInCourseId(id);
+			model.addAttribute("msg", "Avg grade: " + avgGrades);
+			return "grade-show-all-page";//tiks parādīt grade-show-all-page.html lapa interneta pārlūka
+		}
+		catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());//padod kļudas ziņu uz error-page.html lapu
+			return "error-page";//parādām pašu error-page.html lapu interneta pārlūkā
+
+		}
+	}
 	
 	
 	
