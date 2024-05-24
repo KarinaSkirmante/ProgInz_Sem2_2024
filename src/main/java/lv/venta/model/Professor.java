a0d391a7a86a579522bd4f79fc51af45dd534a76
KarinaSkirmante <karina.krinkele@gmail.com>
2024-05-24 09:49:58 +0300

    Update: added person class anad update professor and Student with extends

--- src/main/java/lv/venta/model/Professor.java
+++ src/main/java/lv/venta/model/Professor.java
@@ -22,26 +22,14 @@ import lombok.ToString;
 @ToString
 @Table(name = "ProfessorTable")
 @Entity
-public class Professor {
+public class Professor extends Person{
 	@Id
 	@Column(name = "Idp")
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Setter(value = AccessLevel.NONE)
 	private int idp;
 
-	// TODO uzlabot regex gan vārdam, gan uzvārdam
-	@Column(name = "Name")
-	@NotNull
-	@Size(min = 3, max = 50)
-	@Pattern(regexp = "[A-ZĒŪĪĻĶĢŠĀŽČŅ]{1}[a-zēūīļķģšāžčņ]+", message = "Only letters and space are allowed")
-	private String name;
-
-	@Column(name = "Surname")
-	@NotNull
-	@Size(min = 3, max = 50)
-	@Pattern(regexp = "[A-ZĒŪĪĻĶĢŠĀŽČŅ]{1}[a-zēūīļķģšāžčņ]+", message = "Only letters and space are allowed")
-	private String surname;
-	
+		
 	@NotNull
 	@Column(name="Degree")
 	private Degree degree;
@@ -52,8 +40,7 @@ public class Professor {
 	
 
 	public Professor(String name, String surname, Degree degree) {
-		setName(name);
-		setSurname(surname);
+		super(name, surname);
 		setDegree(degree);
 	}
 }
